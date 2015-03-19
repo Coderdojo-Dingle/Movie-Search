456355cd7d0d70d0021b242abf7c62e024cf67da
Adam Byrne <adam.r.byrne@gmail.com>
2015-03-19 19:48:26 +0000

    took the title away

--- main.js
+++ main.js
@@ -30,42 +30,6 @@ $(function(){
         }else{
           console.log(data);
           for(var i = 0; i < data.results.length; i++) {
-            var good = "";
-            if(data.results[i].vote_average > 4.8) {
-              good = "Good!";
-            }
-
-            if (data.results[i].vote_average > 5.5) {
-             good = "Amazing";
-            }
-
-            if(data.results[i].vote_average < 4.8) {
-              good = "Alright";
-            }
-
-            if(data.results[i].vote_average < 1.0) {
-              good = "Terrible";
-            }
-
-            if(data.results[i].vote_average > 7.5) {
-              good = "Impeccible"
-            }
-
-            if(data.results[i].vote_average < 2.0) {
-              good = "Not so good";
-            }
-
-            if(data.results[i].vote_average > 9.0) {
-              good = "Mesmorising";
-            }
-
-            if(data.results[i].vote_count == 0) {
-              good = "Sorry, no votes!";
-            }
-
-
-
-
             if(data.results[i].poster_path) {
               $('#info').append("<li><img style='width:300px; height:auto;' id='Poster' src='"+baseIMG+ data.results[i].poster_path+"'></li>");
             }else {
